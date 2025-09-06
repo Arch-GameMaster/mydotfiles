35ae785304c50e59ef0e9201be44658cd6db9ae1
Antonio Lugo <mcantonio0913@gmail.com>
2025-09-05 22:31:10 -0600

    Update mypolybar.sh

--- mypolybar.sh
+++ mypolybar.sh
@@ -4,5 +4,6 @@ killall polybar
 
 sleep 5 &
 
-polybar -r -c ~/.config/polybar/bspwm/config.ini top &
+# polybar -r -c ~/.config/polybar/bspwm/config.ini top &
 
+polybar -r -c ~/.config/polybar/i3/config.ini top &
