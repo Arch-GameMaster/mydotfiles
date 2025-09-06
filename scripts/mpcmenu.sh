e0a355f298acdc469a221e95109802012da3c923
Antonio <gamemaster@arch-gamemaster.localdomain>
2025-09-05 20:11:02 -0600

    New update

--- scripts/mpcmenu.sh
+++ scripts/mpcmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-     cat $HOME/.config/jgmenu/menus/mpcmenu | jgmenu --config-file=.config/jgmenu/mymenurc2 --simple --icon-size=0 &
+     cat $HOME/.config/jgmenu/menus/mpcmenu | jgmenu --config-file=.config/jgmenu/mymenurc2 --simple --icon-size=0 --persistent &
