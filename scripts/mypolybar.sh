834147afa87b11e4a9608c5f090b25348b12a2d0
Antonio <gamemaster@arch-gamemaster.localdomain>
2025-09-17 15:38:04 -0600

    New Update

--- scripts/mypolybar.sh
+++ scripts/mypolybar.sh
@@ -4,8 +4,8 @@ killall polybar
 
 sleep 3 &
 
- polybar -r -c ~/.config/polybar/i3/config.ini top &
+# polybar -r -c ~/.config/polybar/i3/config.ini top &
 
-# polybar -r -c ~/.config/polybar/bspwm/config.ini top &
+# polybar -r -c ~/.config/polybar/darkness/config.ini top &
 
-# polybar -r -c ~/.config/polybar/wxp/config.ini bottom &
+ polybar -r -c ~/.config/polybar/wxp/config.ini bottom &
--- scripts/pwrmenu.sh
+++ scripts/pwrmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat ~/.config/jgmenu/pwrmenu | jgmenu --config-file=~/.config/jgmenu/mymenurc --simple
+cat ~/.config/jgmenu/menus/pwrmenu | jgmenu --config-file=~/.config/jgmenu/mymenurc --simple
