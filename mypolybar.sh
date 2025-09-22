aa27c9173e6dcefc26e9ad5a8fae34e61efdd1b1
Antonio Lugo <mcantonio0913@gmail.com>
2025-09-21 18:50:47 -0600

    Add files via upload

---
+++ mypolybar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+killall polybar
+
+sleep 5 &
+
+#polybar -r -c ~/.config/polybar/i3/config.ini top &
+#polybar -r -c ~/.config/polybar/bspwm/config.ini top &
+polybar -r -c ~/.config/polybar/wxp/config.ini bottom &
+
